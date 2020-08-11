b44b24febbc744a57a2a0fe107f8069d72c5929e
jainvaidehi <54447956+jainvaidehi@users.noreply.github.com>
2020-08-11 12:17:03 +0530

    Created Kadane’s Algorithm

---
+++ Kadane_algo.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum=0;
+        int maxx=nums[0];
+        for(int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+            if(sum>maxx)
+                maxx=sum;
+            if(sum<0)
+                sum=0;
+        }
+        return maxx;
+    }
+};
