f7f3c0ed224aab142bbe653efe03be07003bc1a9
jainvaidehi <54447956+jainvaidehi@users.noreply.github.com>
2020-08-27 18:56:40 +0530

    Created Set matrix zero

---
+++ Set_mat_0.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int r=matrix.size();
+        int c=matrix[0].size();
+        int f=0;
+        for(int i=0;i<r;i++)
+        {
+            if(matrix[i][0]==0)
+                f=1;
+            for(int j=1;j<c;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                matrix[0][j]=0;
+                matrix[i][0]=0;
+                }
+            }
+            
+        }
+        for(int i=0;i<r;i++)
+            for(int j=0;j<c;j++)
+              if(matrix[i][0] == 0|| matrix[0][j]==0)
+                  matrix[i][j]=0;
+        
+         if(matrix[0][0] == 0)
+            for(int j = 0 ; j < c; ++j)
+                matrix[0][j] = 0;
+        if(f==1)
+            for(int  i = 0; i<r; ++i)
+                matrix[i][0] = 0;
+        
+        
+    }
+};
